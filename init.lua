2c740f2c195c437170d4f72e5f0f65a90726d50b
Sam Lewis <sam.lewis0602@gmail.com>
2025-03-09 10:35:08 -0400

    init

---
+++ init.lua
@@ -0,0 +1,15 @@
+-- instant movement for built-in window management
+hs.window.animationDuration = 0
+
+hs.loadSpoon("Hammerflow")
+
+spoon.Hammerflow.loadFirstValidTomlFile({
+    "home.toml",
+    "work.toml",
+    "Spoons/Hammerflow.spoon/sample.toml"
+})
+
+if spoon.Hammerflow.hotReload then
+    hs.loadSpoon("ReloadConfiguration")
+    spoon.ReloadConfiguration:start()
+end
