6e70b2c572f9a025357cd504f415bccaefdcbb48
Sam Lewis <sam.lewis0602@gmail.com>
2025-03-19 19:54:45 -0400

    look for home.toml and work.toml in spoon directory in case users put it there next to sample

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: undefined-global
+
 -- instant movement for built-in window management
 hs.window.animationDuration = 0
 
@@ -5,7 +7,9 @@ hs.loadSpoon("Hammerflow")
 spoon.Hammerflow.loadFirstValidTomlFile({
     "home.toml",
     "work.toml",
-    "Spoons/Hammerflow.spoon/sample.toml"
+    "Spoons/Hammerflow.spoon/sample.toml",
+    "Spoons/Hammerflow.spoon/home.toml",
+    "Spoons/Hammerflow.spoon/work.toml",
 })
 if spoon.Hammerflow.auto_reload then
     hs.loadSpoon("ReloadConfiguration")
